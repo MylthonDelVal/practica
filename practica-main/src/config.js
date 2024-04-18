355b5a41503ef841110f3c2291916ecfa7400ce7
Mylthon Marlon Sanchez Del val <delvalmylthon@gmail.com>
2024-04-17 19:50:21 -0600

    Create config.js

---
+++ practica-main/src/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 4000;
+export const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb+srv://users:User1234@test.jbkrcvl.mongodb.net/?retryWrites=true&w=majority&appName=test";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://epicardo.vercel.app/";
